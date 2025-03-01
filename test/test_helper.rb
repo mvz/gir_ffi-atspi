95d7d886bc5d50a480647b6e5b237617d233e78f
Matijs van Zuijlen <matijs@matijs.net>
2025-03-01 18:40:59 +0100

    Give test app more time to boot in CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,10 +7,12 @@ require "minitest/autorun"
 require "aruba/api"
 
 module EndToEndTestHelpers
-  def find_app(name)
+  def find_app(name) # rubocop:disable Metrics/MethodLength
     desktop = Atspi.get_desktop 0
     app = nil
-    4.times do
+
+    6.times do |try|
+      sleep try * 0.1
       desktop.child_count.times.reverse_each do |i|
         child = desktop.get_child_at_index(i)
         break app = child if child.name == name
